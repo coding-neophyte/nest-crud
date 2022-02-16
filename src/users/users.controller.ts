2435995193b124c1ed94dd7428934e5bfcb7a003
Julius Franklin <jjfranklin88@outlook.com>
2022-02-16 13:39:48 -0500

    adding post route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UserController {
-  constructor(userservice: UsersService) {}
+  constructor(private readonly userservice: UsersService) {}
 
   @Get()
   getUsers() {
     return 'Reached Get Route';
+  }
+
+  @Post()
+  insertUser(
+    @Body('name') name: string,
+    @Body('age') age: number,
+    @Body('email') email: string,
+  ) {
+    const userId = this.userservice.insertUser(name, age, email);
+    return { id: userId };
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './users.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UsersService {
   private users: User[] = [];
 
-//   insertUser(name: string, age: string, email: string);
+  insertUser(name: string, age: number, email: string) {
+    const id = uuidv4();
+    const newUser = new User(id, name, age, email);
+    this.users.push(newUser);
+    return id;
+  }
 }
