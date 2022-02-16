b9681c8939d37f351131de3685a351eae9e153de
Julius Franklin <jjfranklin88@outlook.com>
2022-02-16 14:21:32 -0500

    adding getbyid , update and delete routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userservice: UsersService) {}
-
-  @Get()
-  getUsers() {
-    return 'Reached Get Route';
-  }
 
   @Post()
   insertUser(
@@ -18,5 +21,28 @@ export class UserController {
   ) {
     const userId = this.userservice.insertUser(name, age, email);
     return { id: userId };
+  }
+  @Get()
+  getAllUsers() {
+    return this.userservice.getUsers();
+  }
+
+  @Get(':userId')
+  getUser(@Param('userId') userId: string) {
+    return this.userservice.getUser(userId);
+  }
+
+  @Put(':userId')
+  updateUser(
+    @Param('userId') userId: string,
+    @Body('name') name: string,
+    @Body('age') age: number,
+    @Body('email') email: string,
+  ) {
+    return this.userservice.updateUser(userId, name, age, email);
+  }
+  @Delete(':userId')
+  deleteUser(@Param('userId') userId: string) {
+    this.userservice.deleteUser(userId);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,29 @@ export class UsersService {
     this.users.push(newUser);
     return id;
   }
+  getUsers() {
+    return [...this.users];
+  }
+  getUser(id: string) {
+    return this.getUserById(id)[0];
+  }
+  updateUser(id: string, name: string, age: number, email: string) {
+    const [targetUser, index] = this.getUserById(id);
+    const updatedUserParams = { ...targetUser, name, age, email };
+    const updatedUser = new User(
+      id,
+      updatedUserParams.name,
+      updatedUserParams.age,
+      updatedUserParams.email,
+    );
+    this.users[index] = updatedUser;
+  }
+  deleteUser(id: string) {
+    const [target, index] = this.getUserById(id);
+    this.users.splice(index, 1);
+  }
+  private getUserById(id: string): [User, number] {
+    const index = this.users.findIndex((user) => user.id === id);
+    return [this.users[index], index];
+  }
 }
